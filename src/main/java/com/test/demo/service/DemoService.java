75ac3b9f69a7c9ab036bfd6db216c8d3e8587811
lihui <lihui@95081.com>
2019-12-07 11:55:55 +0800

    modify

--- src/main/java/com/test/demo/service/DemoService.java
+++ src/main/java/com/test/demo/service/DemoService.java
@@ -1,9 +1,5 @@
 package com.test.demo.service;
 
-import java.util.Map;
-
-import com.emotte.kernel.base.msg.dto.AjaxDTO;
-
 /**
  * 
  * @date 2019年6月5日
@@ -13,5 +9,4 @@ public interface DemoService {
 
 	String sayHello(String name);
 
-	AjaxDTO doDeal(Map<String, Object> m, String func);
 }
--- src/main/java/com/test/demo/service/impl/DemoServiceImpl.java
+++ src/main/java/com/test/demo/service/impl/DemoServiceImpl.java
@@ -1,14 +1,10 @@
 package com.test.demo.service.impl;
 
 import java.util.Date;
-import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
-import com.emotte.kernel.base.msg.dto.AjaxDTO;
 import com.test.demo.service.DemoService;
-
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * 
@@ -17,7 +13,6 @@ import lombok.extern.slf4j.Slf4j;
  */
 @com.alibaba.dubbo.config.annotation.Service
 @Component
-@Slf4j
 public class DemoServiceImpl implements DemoService {
 
 	@Override
@@ -26,16 +21,6 @@ public class DemoServiceImpl implements DemoService {
 		String r = "This time is :" + new Date() + ",content:" + name;
 		System.out.println("---返回参数：" + r);
 		return r;
-	}
-
-	@Override
-	public AjaxDTO doDeal(Map<String, Object> m, String func) {
-		log.info("参数：{}",m);
-		log.info("方法：{}",func);
-		AjaxDTO ajax = new AjaxDTO();
-		ajax.setStatus(AjaxDTO.SUCCESS);
-		ajax.setMsg(func);
-		return ajax;
 	}
 
 }
